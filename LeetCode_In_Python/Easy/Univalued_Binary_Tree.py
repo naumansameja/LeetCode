7a20223413ae61b5434474c3b201cffc85e55b86
nauman.sameja <nauman.samejaa@gmail.com>
2024-05-10 22:40:29 +0500

    modified univalued binary tree

--- LeetCode_In_Python/Easy/Univalued_Binary_Tree.py
+++ LeetCode_In_Python/Easy/Univalued_Binary_Tree.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def isUnivalTree(self, root):
         def check_tree(root):
@@ -17,22 +17,3 @@ class Solution:
             self.val = root.val
             return check_tree(root)
         return True
-        
-
-
-
-
-root = TreeNode(1)
-root.left = TreeNode(1)
-root.left.left = TreeNode(1)
-root.left.right = TreeNode(1)
-root.right = TreeNode(1)
-# root.left.left = TreeNode(1)
-root.right.right = TreeNode(5)
-s = Solution()
-
-print(s.isUnivalTree(root))
-x = 5
-
-
-print()
