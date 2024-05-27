673caf9d023fc16b3ba8fe23eeaa8e6724edff04
nauman.sameja <nauman.samejaa@gmail.com>
2024-05-27 16:11:15 +0500

    added Array Partition

---
+++ LeetCode_In_Python/Easy/Array_Partition.py
@@ -0,0 +1,7 @@
+class Solution:
+    def arrayPairSum(self, nums):
+        nums.sort()
+        max_sum = 0
+        for i in range(0,len(nums), 2):
+            max_sum += nums[i]
+        return max_sum
