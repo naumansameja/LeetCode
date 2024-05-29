1234fe418d681b44ac0d2608a056cce2963238ce
nauman.sameja <nauman.samejaa@gmail.com>
2024-05-29 15:18:57 +0500

    added list the products ordered in a pair

---
+++ Leetcode_in_SQL/Easy/List_The_Products_Ordered_In_A_Period.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select p.product_name, sum(unit) as unit
+from products p join orders o on p.product_id = o.product_id and order_date
+where o.order_date between '2020-02-01' and '2020-02-29'
+group by p.product_id
+having unit >= 100
