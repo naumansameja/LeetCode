fc366440ffabc2d3a65f04026893bf4aa1e8990f
nauman.sameja <nauman.samejaa@gmail.com>
2024-07-06 16:31:15 +0500

    modified pass the pillow

--- LeetCode_In_Python/Easy/Pass_The_Pillow.py
+++ LeetCode_In_Python/Easy/Pass_The_Pillow.py
@@ -1,9 +1,5 @@
 class Solution:
     def passThePillow(self, n: int, time: int) -> int:
-        if n == time:
-            return n - 1
-        if n > time:
-            return time + 1
         current = time // (n-1)
         if current & 1:
             return n - (time % (n-1))
