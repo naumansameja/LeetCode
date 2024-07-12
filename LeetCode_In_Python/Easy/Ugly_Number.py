cb71587d56149bcd32a1f3ef3eaf05a5186278bf
nauman.sameja <nauman.samejaa@gmail.com>
2024-07-12 17:41:58 +0500

    added ugly number

---
+++ LeetCode_In_Python/Easy/Ugly_Number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        def max_divide(n,d):
+            if n < 1:
+                return False
+            while n % d == 0:
+                n /= d
+
+
+            return n
+
+        n = max_divide(n,2)
+        n = max_divide(n,3)
+        n = max_divide(n,5)
+        return n == 1
