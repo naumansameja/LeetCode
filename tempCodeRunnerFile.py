a53df3081b4a06741074ef50c12fc454fb1cf745
nauman.sameja <nauman.samejaa@gmail.com>
2024-08-25 20:06:09 +0500

    added min cost climbing stairs

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,7 @@
-ial_digits(1000, 13000000000))
-# print('abc')
+if len(cost) == 2:
+        return 0
+    cost.append(0)
+    for i in range(len(cost)):
+        cost[i] += min(cost[i-1], cost[i-2])
+
+    return cost[-1]
