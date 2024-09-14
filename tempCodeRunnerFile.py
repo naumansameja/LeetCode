ae4d8c1ebb33afd88b08d0dc61316db0fac5cb86
nauman.sameja <nauman.samejaa@gmail.com>
2024-09-14 22:40:25 +0500

    added Delete the Middle Node of a Linked List

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,3 @@
-if len(cost) == 2:
-        return 0
-    cost.append(0)
-    for i in range(len(cost)):
-        cost[i] += min(cost[i-1], cost[i-2])
-
-    return cost[-1]
+5)
+# head.next.next.next.next.next = ListNode(6)
+# head.next.next.next.next.next.next = ListNode(7)
